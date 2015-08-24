3aa59a7781c198c0f1052fcfcea91c5b33826b0a
Barry Smith <barry.of.smith@gmail.com>
2015-08-24 11:07:50 +0100

    Added window and OpenGL debug

---
+++ build-linux/libvolume/CMakeFiles/libvolume.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libvolume.dir/src/libvolume/data/voxel.cpp.o"
+  "CMakeFiles/libvolume.dir/src/libvolume/data/voxelfield.cpp.o"
+  "CMakeFiles/libvolume.dir/src/libvolume/window/window.cpp.o"
+  "CMakeFiles/libvolume.dir/src/libvolume/common/io.cpp.o"
+  "liblibvolume.pdb"
+  "liblibvolume.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libvolume.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "stdio.h"
 
 #include "libvolume/data/voxelfield.h"
+#include "libvolume/window/window.h"
+#include "libvolume/common/io.h"
 
 int main(int argc, char* argv[])
 {
-	printf("Hello, World!\n");
+	LibVolume::IO::output("Starting VoxelTest");
 	
 	LibVolume::Data::VoxelField field(glm::ivec3(16, 16, 16));
+	
+	LibVolume::Data::Voxel* voxel = field.getAt(glm::ivec3(0, 0, 0));
+	
+	LibVolume::Window::Window window;
+	window.outputContextDebug();
 	
 	return 0;
 }
