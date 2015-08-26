c227e459ad5d37fbac63f4d235de2c67bd35b27e
Barry Smith <barry.of.smith@gmail.com>
2015-08-26 13:55:16 +0100

    Added new realm and rendering

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "libvolume/data/voxelfield.h"
 #include "libvolume/window/window.h"
 #include "libvolume/common/io.h"
+#include "libvolume/engine/realm.h"
 
 int main(int argc, char* argv[])
 {
@@ -12,8 +13,14 @@ int main(int argc, char* argv[])
 	window.activate(true);
 	window.outputContextDebug();
 	
+	LibVolume::Engine::Realm realm;
+	
 	//Run the window
-	while (window.tick() == false);
+	while (window.tick() == false)
+	{
+		realm.tick();
+		realm.render();
+	};
 	
 	return 0;
 }
