46929f3946d50a312afc719246ab98eac9a98b5c
Barry Smith <barry.of.smith@gmail.com>
2015-08-28 21:45:58 +0100

    Updated with new libvolume rendering features

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,20 @@ int main(int argc, char* argv[])
 	window.outputContextDebug();
 	
 	LibVolume::Engine::Realm realm;
-	LibVolume::Engine::Actor actor;
+	LibVolume::Engine::Actor* actor = new LibVolume::Engine::Actor();
 	
-	actor.mesh.loadFromOBJ("../Dugtrio.obj.test");
+	actor->mesh.loadFromOBJ("../Dugtrio.obj.test");
 	
 	//actor.state.scale = glm::vec3(100.0, 100.0, 100.0);
 	
-	realm.objects.push_back((LibVolume::Engine::Object*)&actor);
+	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(actor));
 	
 	//Run the window
 	while (window.tick() == false)
 	{
 		realm.tick();
 		realm.render();
+		//realm.renderer.renderActor(&actor);
 	};
 	
 	return 0;
