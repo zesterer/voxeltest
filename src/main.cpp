a8dab56362b3d143b9475a59cadbbf6592701029
Barry Smith <barry.of.smith@gmail.com>
2015-08-29 20:25:10 +0100

    Updated for newer features

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,14 @@ int main(int argc, char* argv[])
 	window.outputContextDebug();
 	
 	LibVolume::Engine::Realm realm;
+	realm.setEventManager(&window.event_manager);
 	LibVolume::Engine::Actor* actor = new LibVolume::Engine::Actor();
 	
-	actor->mesh.loadFromOBJ("../Dugtrio.obj.test");
+	actor->mesh.loadFromOBJ("../Link.obj.test");
+	actor->state.position.y = -3.5;
+	actor->state.scale = glm::vec3(0.6, 0.6, 0.6);
+	//actor->state.orientation = glm::quat(glm::vec3(-1.6, 0.0, 0.0));
+	actor->state.spin = glm::quat(glm::vec3(0.0, 0.03, 0.0));
 	
 	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(actor));
 	
