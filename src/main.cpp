39c4b519194f4e0230ed5bc43a62661227ea9c6c
Barry Smith <barry.of.smith@gmail.com>
2015-08-31 22:09:31 +0100

    Added better textures

--- src/main.cpp
+++ src/main.cpp
@@ -17,23 +17,23 @@ int main(int argc, char* argv[])
 	realm.setEventManager(&window.event_manager);
 
 	LibVolume::Engine::Actor* ship = new LibVolume::Engine::Actor();
-	ship->mesh.loadFromOBJ("../Starfighter.obj.test");
-	ship->mesh_state.orientation = glm::quat(glm::vec3(3.1, 0.0, 3.1));
+	ship->mesh.loadFromOBJ("../X-Wing.obj.test");
+	ship->state.position = glm::vec3(50.0, 0.0, 0.0);
+	ship->state.scale = glm::vec3(1.5, 1.5, 1.5);
+	ship->mesh_state.position += glm::vec3(0.0, -20.0, 0.0);
+	ship->mesh_state.orientation = glm::quat(glm::vec3(3.141, 0.0, 3.141));
 	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(ship));
 
 	LibVolume::Engine::Actor* planet = new LibVolume::Engine::Actor();
 	planet->mesh.loadFromOBJ("../Planet0.obj.test");
 	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(planet));
 
-	LibVolume::Render::Structures::Light sun(LibVolume::Render::Structures::LightType::Directional, glm::vec3(0.5, 0.5, -1.0), glm::vec3(1.0, 1.0, 0.9), 0.4);
+	LibVolume::Render::Structures::Light sun(LibVolume::Render::Structures::LightType::Directional, glm::vec3(0.5, 0.5, -1.0), glm::vec3(1.0, 1.0, 0.9), 0.2);
 	realm.light_list.push_back(&sun);
 
 	//Run the window
 	while (window.tick() == false)
 	{
-		realm.tick();
-		realm.render();
-
 		if (window.event_manager.keyboard_state.key_space)
 			ship->state.velocity += ship->state.orientation * glm::vec3(0.0, 0.1, 0.0);
 		if (window.event_manager.keyboard_state.key_shift)
@@ -60,8 +60,14 @@ int main(int argc, char* argv[])
 		if (window.event_manager.keyboard_state.key_e)
 			ship->state.orientation *= glm::quat(glm::vec3(0.0, 0.0, -0.03));// * ship->state.orientation;
 
-		realm.camera.state.position = ship->state.position + glm::vec3(0.0, 0.0, 300.0) * glm::inverse(ship->state.orientation);
+		realm.camera.state.position = ship->state.position + glm::vec3(0.0, 50.0, 300.0) * glm::inverse(ship->state.orientation);
 		realm.camera.state.orientation = glm::inverse(ship->state.orientation);
+
+		//Gravity
+		//ship->state.velocity += glm::normalize(planet->state.position - ship->state.position) * 0.01f;
+
+		realm.tick();
+		realm.render();
 	};
 
 	return 0;
