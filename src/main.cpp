ccf291184e2e4c4bfe0ea5f66e7f9581c1cc296b
Barry Smith <barry.of.smith@gmail.com>
2015-09-02 22:48:50 +0100

    Added new asteroid as voxelbody

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "libvolume/common/io.h"
 #include "libvolume/engine/realm.h"
 #include "libvolume/engine/actor.h"
+#include "libvolume/engine/voxelactor.h"
 
 void addPlanet(LibVolume::Engine::Realm* realm)
 {
@@ -36,25 +37,51 @@ int main(int argc, char* argv[])
 	realm.setEventManager(&window.event_manager);
 
 	LibVolume::Engine::Actor* ship = new LibVolume::Engine::Actor();
-	ship->mesh->loadFromOBJ("../spaceship0.obj.test");
+	ship->mesh->loadFromOBJ("../spaceship0.obj.test", true);
 	ship->state.position = glm::vec3(50.0, 0.0, 0.0);
 	ship->state.scale = glm::vec3(20.0, 20.0, 20.0);
 	ship->mesh_state.orientation = glm::quat(glm::vec3(-1.55, 0.0, 1.55));
 	ship->mesh->colour = glm::vec3(0.5, 0.05, 0.05);
 	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(ship));
 
-	LibVolume::Engine::Actor* planet0 = new LibVolume::Engine::Actor();
+	LibVolume::Engine::VoxelActor asteroid(glm::ivec3(16, 16, 16));
+	asteroid.state.scale = glm::vec3(1000.0, 1000.0, 1000.0);
+	asteroid.mesh_state.position = glm::vec3(-8.0, -8.0, -8.0);
+	asteroid.mesh->colour = glm::vec3(0.5, 0.4, 0.2);
+
+	for (int x = -8; x < 8; x ++)
+	{
+		for (int y = -8; y < 8; y ++)
+		{
+			for (int z = -8; z < 8; z ++)
+			{
+				float d = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+				asteroid.getAt(glm::ivec3(x + 8, y + 8, z + 8))->density = d * (8.0 - std::sqrt(x * x + y * y + z * z));
+			}
+		}
+	}
+
+	float a = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	float b = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	float c = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+	asteroid.state.spin = glm::quat(glm::vec3(2.0 * a - 1.0, 2.0 * b - 1.0, 2.0 * c - 1.0) * 0.01f);
+
+	asteroid.extractMarchingCubes();
+	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(&asteroid));
+
+	/*LibVolume::Engine::Actor* planet0 = new LibVolume::Engine::Actor();
 	planet0->mesh->loadFromOBJ("../earth.obj.test");
 	planet0->mesh->colour = glm::vec3(0.0, 0.7, 0.0);
 	planet0->state.scale = glm::vec3(5000.0, 5000.0, 5000.0);
-	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(planet0));
+	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(planet0));*/
 
 	LibVolume::Engine::Actor* sky = new LibVolume::Engine::Actor();
 	sky->mesh->loadFromOBJ("../skybox.obj.test");
 	sky->mesh_state.scale = glm::vec3(100.0, 100.0, 100.0);
 	realm.objects.push_back(dynamic_cast<LibVolume::Engine::Object*>(sky));
 
-	for (int count = 0; count < 10; count ++)
+	for (int count = 0; count < 4; count ++)
 		addPlanet(&realm);
 
 	LibVolume::Render::Structures::Light sun(LibVolume::Render::Structures::LightType::Directional, glm::vec3(0.5, 0.5, -1.0), glm::vec3(1.0, 1.0, 0.9), 0.5);
@@ -94,7 +121,7 @@ int main(int argc, char* argv[])
 		realm.camera.state.orientation = glm::inverse(glm::mix(ship->state.spin, glm::quat(), 8.0f) * ship->state.orientation);
 		realm.camera.state.position = ship->state.position + glm::vec3(0.0, 50.0, 300.0) * realm.camera.state.orientation;
 
-		ship->state.spin = glm::mix(ship->state.spin, glm::quat(), 0.015f * ship->state.spin.w);
+		ship->state.spin = glm::mix(ship->state.spin, glm::quat(), 0.015f + 0.010f * ship->state.spin.w);
 
 		sky->state.position = ship->state.position;
 
